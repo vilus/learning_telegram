414abeb79129f533da250fef626dbb69efc329fc
Vladimir Shevchenko <shevchenko_for_hr@mail.ru>
2018-01-18 08:59:03 -0500

    added several commands with buttons
    /menu_on  - show buttons with command /say "something"
    /menu_off - hide below buttons
    /links - inline buttons with urls

--- handlers.py
+++ handlers.py
@@ -3,10 +3,18 @@ from telegram.ext import Dispatcher
 from telegram.ext import CommandHandler
 from telegram.ext import Filters
 from telegram.ext import MessageHandler
+from telegram import ReplyKeyboardMarkup
+from telegram import ReplyKeyboardRemove
+from telegram import InlineKeyboardButton
+from telegram import InlineKeyboardMarkup
 
 
 def start(bot, update):
     bot.sendMessage(chat_id=update.message.chat_id, text='Hi')
+
+
+def unknown_cmd(bot, update):
+    bot.sendMessage(chat_id=update.message.chat_id, text='Unknown command: {0}'.format(update.message.text))
 
 
 def echo(bot, update, args=None):
@@ -17,10 +25,34 @@ def echo(bot, update, args=None):
     bot.sendMessage(chat_id=update.message.chat_id, text=args)
 
 
+def menu_on(bot, update):
+    custom_keyboard = [['/say Hey', '/say Hi'], ['/say Ho', '/say Hyi']]
+    #                    top-left   top-right   bottom-left  bottom-right
+    reply_markup = ReplyKeyboardMarkup(custom_keyboard)
+    bot.sendMessage(chat_id=update.message.chat_id, text='Menu on', reply_markup=reply_markup)
+
+
+def menu_off(bot, update):
+    reply_markup = ReplyKeyboardRemove()
+    bot.sendMessage(chat_id=update.message.chat_id, text="Menu off", reply_markup=reply_markup)
+
+
+def links(bot, update):
+    button_list = [[
+        InlineKeyboardButton('bash.im', url='http://bash.im'),
+        InlineKeyboardButton('YouTube', url='https://www.youtube.com')
+    ]]
+    reply_markup = InlineKeyboardMarkup(button_list)
+    bot.sendMessage(chat_id=update.message.chat_id, text='Favorite links', reply_markup=reply_markup)
+
+
 def get_dispatcher(bot):
     dp = Dispatcher(bot, None, workers=0)
     dp.add_handler(CommandHandler('start', start))
     dp.add_handler(CommandHandler('say', echo, pass_args=True))
+    dp.add_handler(CommandHandler('menu_on', menu_on))
+    dp.add_handler(CommandHandler('menu_off', menu_off))
+    dp.add_handler(CommandHandler('links', links))
     dp.add_handler(MessageHandler(Filters.text, echo))
-    dp.add_handler(MessageHandler(Filters.command, echo))  # TODO: handle unknown command
+    dp.add_handler(MessageHandler(Filters.command, unknown_cmd))
     return dp
